54527a3ef64b3557fa1e23fe8b2e006e3b9e4c21
sylvia <75647306+silkycell@users.noreply.github.com>
2024-10-10 01:13:19 -0400

    add icons for unsaved and recovered projects

--- menu/projects/project_button.gd
+++ menu/projects/project_button.gd
@@ -6,6 +6,9 @@ const THUMBNAIL_MISSING = preload("res://menu/projects/thumbnail_missing.png")
 @export var label:Label
 @export var thumbnail:TextureRect
 
+@export var recovered_icon:TextureRect
+@export var unsaved_icon:TextureRect
+
 var project:Project
 
 var thumbnail_thread:Thread
@@ -13,6 +16,9 @@ var thumbnail_thread:Thread
 func load(project_to_load:Project):
 	project = project_to_load
 	label.text = project.name
+	
+	recovered_icon.visible = project.is_recovered
+	unsaved_icon.visible = !project.is_previously_saved
 	
 	if project.thumbnail == null:
 		await project.thumbnail_finished_loading
